e94ea07c1f3aeac8e377a47395dc13ec03842b8f
Italo Queiroz <batutista14@gmail.com>
2017-08-11 10:12:08 -0300

    Initial commit

---
+++ app/src/main/java/br/com/italoqueiroz/escolapp/MainActivity.java
@@ -0,0 +1,21 @@
+package br.com.italoqueiroz.escolapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button x = (Button) findViewById(R.id.buttonCadastrar);
+    }
+    public void executarCadastrar(View paramentro){
+        Toast.makeText(this, "texto", Toast.LENGTH_LONG);
+    }
+}
