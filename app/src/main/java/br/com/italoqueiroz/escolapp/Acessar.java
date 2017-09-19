2267156c420cce8e08152c83a062e588d4550000
Italo Queiroz <batutista14@gmail.com>
2017-09-19 01:06:38 -0300

    Adicionado a Activity Mapas e um botão Mapas na tela Acessar.

--- app/src/main/java/br/com/italoqueiroz/escolapp/Acessar.java
+++ app/src/main/java/br/com/italoqueiroz/escolapp/Acessar.java
@@ -1,7 +1,10 @@
 package br.com.italoqueiroz.escolapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Acessar extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Acessar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acessar);
+
+        Button botaoMapas = (Button) findViewById(R.id.buttonMapas);
+        botaoMapas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mapas = new Intent(Acessar.this, Mapas.class);
+                startActivity(mapas);
+            }
+        });
     }
 }
